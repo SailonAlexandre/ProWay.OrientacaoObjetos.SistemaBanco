ba4d32a8179b55a14bc8db2ff660243ab13d1c88
José Klaumann <jose.augusto.klaumann@hotmail.com>
2021-12-14 18:30:23 -0300

    Add project files.

---
+++ ProWay.OrientacaoObjetos.Models/ContaBancaria/ContaPoupanca.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProWay.OrientacaoObjetos.Models.ContaBancaria
+{
+    public class ContaPoupanca : Conta
+    {
+        // Construtor é uma forma de garantir que o objeto
+        // é construído da forma correta, ou seja, neste
+        // cenário para abrir uma conta poupança é obrigatório
+        // ter nome e saldo da conta antes de conseguir abrir
+        // efetivamente
+        public ContaPoupanca(string nome, double saldo)
+        {
+            Nome = nome;
+            Saldo = saldo;
+        }
+    }
+}
